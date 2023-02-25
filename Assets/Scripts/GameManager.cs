0c12bbc87985771766e27e9a7f4d597e71de25d5
Matt Doak <contact@mattdoak.ca>
2023-02-25 13:51:53 -0600

    Hooked up conductor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public InterfaceManager ui;
+    public Conductor conductor;
+    
+    [SerializeField]
+    public List<AudioClip> roundSongs = new List<AudioClip>();
         
     private int _currRound = 0;
     private const int NumRounds = 10;
     private int _remainingEnemies = 0;
+    private int _enemiesSpawned = 0;
     private const int EnemiesPerWave = 100;
+
+    public GameObject prefabEnemy;
     public PlayerUnit player; 
-    
 
     void Start()
     {
@@ -27,6 +34,9 @@ public class GameManager : MonoBehaviour
     {
         yield return ui.StartRound(_currRound);
         _remainingEnemies = EnemiesPerWave;
+        
+        conductor.LoadSong(new Song(roundSongs[_currRound - 1 % roundSongs.Count], 120));
+        conductor.Play();
 
         while (player.IsAlive && _remainingEnemies > 0)
         {
