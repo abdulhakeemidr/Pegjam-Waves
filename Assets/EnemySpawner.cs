8728fced5ba1dba419159c8459743e08c057ee50
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2023-02-25 16:06:09 -0600

    Enemy Spawner Basic

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject enemyPrefab;
+    public float distanceFromEdge = 1f; //distance of the object from the edge of the screen
+
+    void Start()
+    {
+        SpawnObject(Screen.width / 2f, Screen.height - distanceFromEdge); //spawn at top center
+        SpawnObject(Screen.width - distanceFromEdge, Screen.height / 2f); //spawn at right center
+        SpawnObject(Screen.width / 2f, distanceFromEdge); //spawn at bottom center
+        SpawnObject(distanceFromEdge, Screen.height / 2f); //spawn at left center
+    }
+
+    void SpawnObject(float x, float y)
+    {
+        Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0f));
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
 }
