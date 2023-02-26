1a0fef8a32b63f9781491bcab2c4422b9de3ec1f
Cameron Jung <c.jung@mymts.net>
2023-02-26 12:59:03 -0600

    Made shots scale in power according the beat

--- Assets/Scripts/SoundBlast.cs
+++ Assets/Scripts/SoundBlast.cs
@@ -7,9 +7,15 @@ public class SoundBlast : MonoBehaviour
 
     public float speed = 50.0f;
     public float lifeTime = 0.5f;
-    public int damage = 1;
+    public int minDamage = 1;
     private Rigidbody2D body;
-    
+
+    private int damage = 1;
+    private float size = 1.0f;
+
+    [SerializeField] int maxDamage = 10;
+    [SerializeField] int maxPenetration = 5;
+    [SerializeField] float maxSize = 5.0f;
 
 
     private void OnEnable()
@@ -23,6 +29,7 @@ public class SoundBlast : MonoBehaviour
     {
         body.AddRelativeForce(new Vector2(1.0f, 0.0f) * speed, ForceMode2D.Impulse);
         Invoke("CleanUp", lifeTime);
+
     }
 
     // Update is called once per frame
@@ -31,8 +38,27 @@ public class SoundBlast : MonoBehaviour
         
     }
 
+    public void AdjustPower(float resonance)
+    {
+        damage = Mathf.Max(minDamage, Mathf.RoundToInt(resonance * resonance * maxDamage));
+        transform.localScale = transform.localScale * Mathf.Max(1.0f, (resonance * resonance * maxSize));
+    }
+
     private void CleanUp()
     {
         Destroy(gameObject);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //Deal damage to enemy
+        }
+        else
+        {
+            CleanUp();
+        }
+    }
+
 }
